c9463ca3b1d83d3836dfc74bf0db2a00c23eeddf
JJ7ECNO <jj7ecno@gmail.com>
2020-11-30 16:46:42 -0300

    execício variáveis

---
+++ semana3/variaveis/src/index.js
@@ -0,0 +1,40 @@
+/* RESOSTAS DOS EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO
+ Exercício 1:
+ console.log (b) = 10
+ console.log (a, b) = 10, 5
+
+ Exercício 2:
+ console.log(a, b, c) = 10, 10, 10,*/
+
+
+
+// const nome = "Jessica"
+// const sobrenome = "Damaceno"
+
+// console.log(nome, sobrenome)
+
+let nome
+let idade
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+// Foi impresso um tipo undefined, pois não havia um valor na variável.
+
+nome = prompt("Qual o seu nome?")
+idade = prompt("Qual a sua idade?")
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+//Notei que saiu para mim informação de duas strings, pois ambas variáveis estavam entre aspas, tanto nome quanto número.
+
+
+console.log("Olá ",nome,", você tem ",idade," anos.")
+
+
+
+
+
+
+
