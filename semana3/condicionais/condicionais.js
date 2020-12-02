890921b89fdf49a1afe52e31ac709b6491031532
JJ7ECNO <jj7ecno@gmail.com>
2020-12-02 17:00:47 -0300

    exercicio condicionais

---
+++ semana3/condicionais/condicionais.js
@@ -0,0 +1,30 @@
+// EXERCÍCOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// Exercício 1
+
+// Exercício 2
+//a) para informar o preços das frutas que estão a venda
+//b) 2.25
+//c) Ele imprime 5.5 o valor de "Pera"
+
+// Exercicio 3
+//a) A primeira linha pede para @ usuari@ digitar o primeiro numero, porém, transformando a resposta string em número.
+//b) A mensagem para um numero 10, será "Esse número passou no teste". Se fosse -10, não seria impressa nenhuma mensagem.
+//c) Sim. Como a variável de if é pai (scopo global), ela não tem acesso a informação que nestá dentro da variavel filho, então este não estari acessível, a não ser que estivesse dentro do escopo global.
+
+
+// EXERCÍCIO DE ESCRITA DE CÓDIGO
+
+// Exercicio 4
+//1. 2. 
+// let idadePessoa = Number(prompt("Digite aqui a sua idade"))
+
+// if (idadePessoa >= 18) {
+//     console.log("Você pode dirigir")
+//     alert(`Você pode dirigir`)
+// } else {
+//     console.log("Você não pode dirigir")
+//     alert(`Você não pode dirigir`)
+// }
+
+// Exercício 5
