635760ba394d01173402c6ea7723114b34a32b47
JJ7ECNO <jj7ecno@gmail.com>
2020-12-03 16:54:44 -0300

    exercício loops

---
+++ semana3/loops/loops.js
@@ -0,0 +1,35 @@
+//EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO
+
+// Exercício 1
+
+//Está criando uma repetição até que o valor de i seja menor que 5
+
+// Exercício 2
+// a)
+//Está criando uma repetição guardando na lista numeros maiores que 18, até que chegue no maior numero da lista.
+// b)
+//Para acessar o índice de cada elemento o for...of... é suficiente. Seria feito utilizando:
+
+// Para: 
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// for (let numero of lista) {
+//   if (numero > 18) {
+// 		console.log(numero)
+// 	}
+// }
+
+// Seria feito assim:
+
+// for(let i = 0; 1 < numeros.length; i++) {
+//     if (numero[i] > maior) {
+//         maior = numeros [i] 
+//     }
+// }
+
+// console.log("O amior numero é", maior)
+
+// Desafio 1
+
+
+
+//EXERCÍCIO DE ESCRITA DE CÓDIGO
