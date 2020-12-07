f93f7f3d24af2aa781d2794e8e51fe043655ff9f
JJ7ECNO <jj7ecno@gmail.com>
2020-12-07 16:50:26 -0300

    exercício funcões

---
+++ semana4/funcoes/src/index.js
@@ -0,0 +1,35 @@
+
+// ----------------------------------**Exercícios de interpretação de código**---------------------------------- //
+
+// ---> EXERCÍCIO 1
+
+//a. vai retornar o resulado das equações: 2*5 e 10*5.
+//b. não iria aparecer nada no console, pois a função não foi chamada/invocada.
+
+
+// ---> EXERCÍCIO 2
+
+//a. Darvas
+//b. Seria impresso "Amanda"
+
+
+// ---> EXERCÍCIO 3      ---------------------------------------(AINDA NÃO FEITO)
+
+//
+
+
+// ----------------------------------**Exercícios de escrita de código**---------------------------------- //
+
+
+// ---> EXERCÍCIO 4
+//a.
+// let sobreMim = () => {
+//     console.log("Eu sou Jess, tenho 28 anos, moro em Salvador e sou estudante.")
+// }
+
+// sobreMim()
+
+//b. 
+// function sobreMim(){
+
+// }
