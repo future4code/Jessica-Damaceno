29f85e2e296734250a49ddcb6792c0aca20590f4
JJ7ECNO <jj7ecno@gmail.com>
2020-12-07 21:10:28 -0300

    exercícios funções

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -1,4 +1,3 @@
-
 // ----------------------------------**Exercícios de interpretação de código**---------------------------------- //
 
 // ---> EXERCÍCIO 1
@@ -15,7 +14,10 @@
 
 // ---> EXERCÍCIO 3      ---------------------------------------(AINDA NÃO FEITO)
 
-//
+// ele tenta fazer um calculo ao quadrado e colocara o reusltaod no arrey
+//o operador de porcentagem vai pegar essa divisão e cada vez q a divisão por 
+// pares,  o resto for zero, ele vai dar esse resultado de dois numero iguais
+// será retornado um valor final de dois numeros iguais
 
 
 // ----------------------------------**Exercícios de escrita de código**---------------------------------- //
@@ -26,10 +28,59 @@
 // let sobreMim = () => {
 //     console.log("Eu sou Jess, tenho 28 anos, moro em Salvador e sou estudante.")
 // }
-
+ 
 // sobreMim()
 
 //b. 
-// function sobreMim(){
+// function sobreMim (nome, idade, cidade, estudante){
+//       if (estudante === true){
+//         estudante = "sou"
+//       } else{
+//         estudante = "não sou"
+//       }
+//         console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade}, ${estudante} estudante`)
+//   }
 
-// }
+//   sobreMim("Jess", 28, "Salvador", true)
+
+// ---> EXERCÍCIO 5
+
+//a. 
+    // let somaNumeros = function (num1, num2){
+    //     let soma = num1 + num2
+    //     return soma
+    // }
+    
+    // let resultado = somaNumeros(20, 8)
+    // console.log(resultado)
+
+//b.
+    // function analisarNum (num1, num2) {
+    //     if (num1 >= num2) {
+    //         const numMaior = true
+    //         return numMaior
+    //     }   
+    // }
+    // console.log(analisarNum(48, 28))
+
+
+//c.
+    // function cumprimentar(){
+    //     console.log("Oi, tudo bem?")
+    // }
+    //     for(let i = 10; i !== 0; i--){
+    //     cumprimentar()
+    // }
+
+
+// ---> EXERCÍCIO 6
+
+//a 
+    // const array = [10, 23, 45, 78, 90, 52, 35, 67, 84, 22, 50, 28, 30, 11, 7, 3];
+
+    // function contarElementos(arrayNum){
+    // return arrayNum.length
+    // };
+
+    // console.log(`O numero de elementos nesta array é:`, contarElementos(array));
+
