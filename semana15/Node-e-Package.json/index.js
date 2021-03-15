82edd32c3556a7318d03d999f1cbe710b7915be5
JJ7ECNO <71451596+JJ7ECNO@users.noreply.github.com>
2021-03-15 16:52:06 -0300

    exercicio 1

--- semana15/Node-e-Package.json/index.js
+++ semana15/Node-e-Package.json/index.js
@@ -1,4 +1,15 @@
 // Exercício 1
 // a) Para acessar os parâmetros na linha de comando para o Node, utilizamos a propriedade process.argv.
-// b)
 
+// b)
+// const idade = Number(process.argv[3]);
+
+// console.log(`Olá, ${process.argv[2]}, você tem ${idade} anos.`);
+
+// c)
+// const idade = Number(process.argv[3]);
+// const novaIdade = Number((process.argv[4] = Number(process.argv[3]) + 7));
+
+// console.log(
+//   `Olá, ${process.argv[2]}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}.`
+// );
