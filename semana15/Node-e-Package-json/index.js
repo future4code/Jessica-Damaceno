7c0dad2ec57e8c0470158e17f5496429c965fe27
JJ7ECNO <71451596+JJ7ECNO@users.noreply.github.com>
2021-03-15 21:00:17 -0300

    Exercício 3

---
+++ semana15/Node-e-Package-json/index.js
@@ -0,0 +1,56 @@
+// Exercício 1
+// a) Para acessar os parâmetros na linha de comando para o Node, utilizamos a propriedade process.argv.
+
+// b)
+// const idade = Number(process.argv[3]);
+
+// console.log(`Olá, ${process.argv[2]}, você tem ${idade} anos.`);
+
+// c)
+// const idade = Number(process.argv[3]);
+// const novaIdade = Number((process.argv[4] = Number(process.argv[3]) + 7));
+
+// console.log(
+//   `Olá, ${process.argv[2]}! Você tem ${idade} anos. Em sete anos você terá ${novaIdade}.`
+// );
+
+// Exercício 2
+
+// const opMat = process.argv[2];
+// const x = Number(process.argv[3]);
+// const y = Number(process.argv[4]);
+
+// switch (opMat) {
+//   case "soma":
+//     console.log(x + y);
+//     break;
+//   case "sub":
+//     console.log(x - y);
+//     break;
+//   case "mult":
+//     console.log(x * y);
+//     break;
+//   case "div":
+//     console.log(x / y);
+//     break;
+// }
+
+// Exercício 3
+
+const tarefas = ["Fazer comida", "Estudar", "Arrumar a casa"];
+const novaTarefa = process.argv[2];
+
+const listaDeTarefas = tarefas.push(novaTarefa);
+console.table("Tarefa adicionada com sucesso! Tarefas:");
+console.table(tarefas);
+
+//Resultado lindo! <3
+//Tarefa adicionada com sucesso! Tarefas:
+// ┌─────────┬──────────────────┐
+// │ (index) │      Values      │
+// ├─────────┼──────────────────┤
+// │    0    │  'Fazer comida'  │
+// │    1    │    'Estudar'     │
+// │    2    │ 'Arrumar a casa' │
+// │    3    │    'estudar+'    │
+// └─────────┴──────────────────┘
