0af06b5d6fbbd7562da15d934f8726ea57b921c7
JJTECNO <JJ7ECNO@gmail.com>
2022-02-01 17:52:19 -0300

    EXERCÍCIO 01

---
+++ modulo1/lista-revisao-js/exercicios.js
@@ -0,0 +1,98 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+
+// EXERCÍCIO 01
+//array: [0,8,23,16,10,15,41,12,13]
+//Esperada: 3
+function retornaTamanhoArray(array) {
+   const tamanhoArray = array.length
+
+    //console.log(tamanhoArray) // apenas para testar
+   return tamanhoArray
+}
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+
+}
+
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  
+}
+
+
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+ 
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+  
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
