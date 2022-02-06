44556f8006f7b54d0715c74dfe1b3ddd644d71f7
JJTECNO <JJ7ECNO@gmail.com>
2022-02-06 16:02:41 -0300

    rendering list and styling

---
+++ modulo1/exercicios-reactjs/exercicios-react/exercicio02-renderizar-lista/src/components/CardName/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function index({name, height}) {
+  return (
+    
+      <li> 
+        <p>
+          {name}
+        </p>
+        <p>
+          {height}
+        </p>
+      </li>
+      
+  );
+};
